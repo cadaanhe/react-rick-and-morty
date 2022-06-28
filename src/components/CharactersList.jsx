291172d639d0808e345033e1470c5e51659cc3eb
cadaanhe <cadaanhe@gmail.com>
2022-06-28 18:59:14 -0500

    first commit

---
+++ src/components/CharactersList.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import Character from "./Character";
+
+function NavPag(props) {
+  return (
+    <header>
+      <p className="d-flex justify-content-center">Page: {props.page}</p>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => props.setPage(props.page - 1)}
+        >
+          Page {props.page - 1}
+        </button>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => props.setPage(props.page + 1)}
+        >
+          Page {props.page + 1}
+        </button>
+      </div>
+    </header>
+  );
+}
+
+function CharacterList() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${page}`
+      );
+      const data = await response.json();
+      setLoading(false);
+      setCharacters(data.results);
+    }
+    fetchData();
+  }, [page]);
+  return (
+    <div className="container">
+      <NavPag page={page} setPage={setPage} />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="row">
+          {characters.map((character) => {
+            return (
+              <div className="col md-4" key={character.id}>
+                <Character character={character} />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <NavPag page={page} setPage={setPage} />
+    </div>
+  );
+}
+
+export default CharacterList;
